53e81b0837c28e488cbd8dab2f125ef17b213859
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-17 17:54:08 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -1,1 +1,15 @@
+image = streamlit.file.uploader("Upload an image")
 
+if image is not None:
+    bytes_data=image.getvalue()
+    st.write(bytes_data)
+
+client = boto3.client('rekognition', 
+                    aws_access_key_id = aws_access_key_id,
+                    aws_secret_access_key= aws_secret_access_key)
+
+with open(image, 'rb') as source_image:
+    source_bytes=source_image.read()
+
+response = client.recognize_celebrities(bytes_data)
+streamlit.write(response['Name'])
