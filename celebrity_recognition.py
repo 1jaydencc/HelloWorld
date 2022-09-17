98f5e6480df28da802fd9cb0691164454b00112e
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-17 18:06:04 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -8,8 +8,8 @@ if image is not None:
     st.write(bytes_data)
 
 client = boto3.client('rekognition', 
-                    aws_access_key_id = st.secrets["aws_access_key_id"],
-                    aws_secret_access_key= st.secrets["aws_secret_access_key"])
+                    aws_access_key_id = streamlit.secrets["aws_access_key_id"],
+                    aws_secret_access_key= streamlit.secrets["aws_secret_access_key"])
 
 with open(image, 'rb') as source_image:
     source_bytes=source_image.read()
