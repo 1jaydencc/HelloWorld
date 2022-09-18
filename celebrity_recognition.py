d062c8be88e51360e67b54e083f452c13d75b9ee
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 02:27:40 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -5,9 +5,10 @@ image = streamlit.file_uploader("Upload an image")
 
 client = boto3.client('rekognition', region_name='us-west-2', aws_access_key_id = streamlit.secrets["aws_access_key_id"], aws_secret_access_key= streamlit.secrets["aws_secret_access_key"])
 
-with open(image, 'rb') as image_file:
-    content = image_file.read()
+if st.button('Analyze'):
+    with open(image, 'rb') as image_file:
+        content = image_file.read()
 
-response = client.recognize_celebrities(Image = {'Bytes': content}, MaxLabels = 10)
+    response = client.recognize_celebrities(Image = {'Bytes': content}, MaxLabels = 10)
 
-streamlit.write(response)
+    streamlit.write(response)
