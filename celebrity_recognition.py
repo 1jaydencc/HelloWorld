85835fde29cbd11fefd0aaca3fc0ea6cf0320387
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 09:02:47 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -18,13 +18,11 @@ if st.button('Analyze'):
     file_img = BytesIO(img)
     content=file_img.read()
     response = client.recognize_celebrities(Image = {'Bytes': content})
-#    if 'CelebrityFaces' in response:
- #       if 0 in response['CelebrityFaces']:
-  #          if 'Name' in response['CelebrityFaces'][0]:
-   #             st.write("Name:", response['CelebrityFaces'][0]['Name'])
-    #            st.write("IMDb Profile:" , response['CelebrityFaces'][0]['Urls'][1])
-    #else: 
-     #   st.write("No celebrity detected.")
-    st.write(response)
-    st.write('CelebrityFaces' in response)
+    if 'CelebrityFaces' in response:
+        if 0 in response['CelebrityFaces']:
+            if 'Name' in response['CelebrityFaces'][0]:
+                st.write("Name:", response['CelebrityFaces'][0]['Name'])
+                st.write("IMDb Profile:" , response['CelebrityFaces'][0]['Urls'][1])
+    else: 
+        st.write("No celebrity detected.")
     
