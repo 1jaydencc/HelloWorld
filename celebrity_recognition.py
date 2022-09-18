57250a15cd297aaa6b1acebeb37d07b0b03e1937
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 02:55:51 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -15,7 +15,6 @@ client = boto3.client('rekognition', region_name='us-west-2', aws_access_key_id 
 if streamlit.button('Analyze'):
     content=file_img.read()
     response = client.recognize_celebrities(Image = {'Bytes': content})
-    streamlit.write(response)
     json_data = json.load(response)
     name = json_data["CelebrityFaces"][0]["Name"]
     imdb = json_data["CelebrityFaces"][0]["Urls"][1]
