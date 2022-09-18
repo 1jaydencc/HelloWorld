f109237efa12c4e8e52682a862c9980ff9580c3b
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 09:37:41 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -21,6 +21,10 @@ if st.button('Analyze'):
     if 'CelebrityFaces' in response and 0 < len(response['CelebrityFaces']) and 'Name' in response['CelebrityFaces'][0]:
         for i in range(len(response['CelebrityFaces'])):
             st.write("Name:", response['CelebrityFaces'][i]['Name'])
+            if 'Urls' in response['CelebrityFaces'][0] and 1 < len(response['CelebrityFaces'][0]['Urls']):
+                st.write("IMDb Profile:" , response['CelebrityFaces'][i]['Urls'][1])
+            else: 
+                st.write("No IMDb profile link in database.")
             
     else: 
         st.write("No celebrity detected.")
