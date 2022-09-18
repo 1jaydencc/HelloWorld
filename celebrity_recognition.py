cdf564b49018163c6a7a2663a8522763f2448c90
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 03:53:36 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -14,8 +14,8 @@ if streamlit.button('Analyze'):
     file_img = BytesIO(img)
     content=file_img.read()
     response = client.recognize_celebrities(Image = {'Bytes': content})
-    if response is not None:
-        streamlit.write("Name:", response['CelebrityFaces'][0]['Name'])
-        streamlit.write("IMDb Profile:" , response['CelebrityFaces'][0]['Urls'][1])
+    #if response is not None:
+     #   streamlit.write("Name:", response['CelebrityFaces'][0]['Name'])
+     #   streamlit.write("IMDb Profile:" , response['CelebrityFaces'][0]['Urls'][1])
     streamlit.write(response)
     
