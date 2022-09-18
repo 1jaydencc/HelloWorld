4c8d9fab54e8a804903cfc16eebdbe2503ba9883
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 03:49:33 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -3,9 +3,9 @@ import streamlit
 from io import BytesIO
 
 image_file = streamlit.file_uploader("Upload an image")
-streamlit.write("If you get an error, either you didn't use a JPG or PNG, or the image does not contain a celebrity face. I know, it's a work in progress")
+streamlit.write("If you get an error, you either didn't use a JPG or PNG, the image is invalid, or the image does not contain a celebrity face. I know, it's a work in progress.")
 
-if image_file is .jpg or .png:
+if image_file is not None:
     img = image_file.read()
 
 client = boto3.client('rekognition', region_name='us-west-2', aws_access_key_id = streamlit.secrets["aws_access_key_id"], aws_secret_access_key= streamlit.secrets["aws_secret_access_key"])
