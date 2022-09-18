cf0e277517730d2de628d91e047d0f2dd8f12855
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2022-09-18 08:55:08 -0400

    Update celebrity_recognition.py

--- celebrity_recognition.py
+++ celebrity_recognition.py
@@ -1,28 +1,29 @@
 import boto3
-import streamlit
+import streamlit as st
 from io import BytesIO
 
-streamlit.title('Celebrity Recognizer')
-streamlit.subheader('By Jayden Cheung')
+st.title('Celebrity Recognizer')
+st.subheader('By Jayden Cheung')
+st.write("The celebrity recognizer will recognize any celebrity from an image as long as their face is prominent. Just upload an image and we'll do the rest")
 
-image_file = streamlit.file_uploader("Upload an image")
-streamlit.write("If you get an error, make sure you use a valid JPG or PNG.")
+image_file = st.file_uploader("Upload an image")
+st.write("Make sure you use a valid JPG or PNG. Other image types will give you an error.")
 
 if image_file is not None:
     img = image_file.read()
 
 client = boto3.client('rekognition', region_name='us-west-2', aws_access_key_id = streamlit.secrets["aws_access_key_id"], aws_secret_access_key= streamlit.secrets["aws_secret_access_key"])
 
-if streamlit.button('Analyze'):
+if st.button('Analyze'):
     file_img = BytesIO(img)
     content=file_img.read()
     response = client.recognize_celebrities(Image = {'Bytes': content})
     if 'Celebrity Faces' in response:
         if 0 in response['CelebrityFaces']:
             if 'Name' in response['CelebrityFaces'][0]:
-                streamlit.write("Name:", response['CelebrityFaces'][0]['Name'])
-                streamlit.write("IMDb Profile:" , response['CelebrityFaces'][0]['Urls'][1])
+                st.write("Name:", response['CelebrityFaces'][0]['Name'])
+                st.write("IMDb Profile:" , response['CelebrityFaces'][0]['Urls'][1])
     else: 
-        streamlit.write("No celebrity")
+        st.write("No celebrity")
 
     
